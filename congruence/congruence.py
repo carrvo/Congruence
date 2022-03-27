c0fb27c9a92069af8cbd921735329537d7928678
carrvo <carrvo@gmail.com>
2022-03-27 12:49:53 -0600

    remove tuple bloat

--- congruence/congruence.py
+++ congruence/congruence.py
@@ -1,20 +1,16 @@
 """
 """
 
-class Congruence(tuple):
+class Congruence(object):
     """
     """
-
-    def __new__(cls, value, modulus):
-        """
-        :kudos: to https://stackoverflow.com/a/23878346
-        """
-        self = super(Congruence, cls).__new__(cls, [value, modulus])
-        return self
 
     def __init__(self, value, modulus):
         self.remainder = value % modulus
         self.modulus = modulus
+
+    def __repr__(self):
+        return repr(tuple([self.remainder, self.modulus]))
 
     def __str__(self):
         return f"{self.remainder} (mod {self.modulus})"
