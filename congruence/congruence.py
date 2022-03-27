c35949de35c3a3a20e04db2bd24957ec14e95dd5
carrvo <carrvo@gmail.com>
2022-03-27 08:54:03 -0600

    user friendly

--- congruence/congruence.py
+++ congruence/congruence.py
@@ -11,3 +11,10 @@ class Congruence(tuple):
         """
         self = super(Congruence, cls).__new__(cls, [value, modulus])
         return self
+
+    def __init__(self, value, modulus):
+        self.value = value
+        self.modulus = modulus
+
+    def __str__(self):
+        return f"{self.value} (mod {self.modulus})"
--- test/test_congruence.py
+++ test/test_congruence.py
@@ -4,6 +4,8 @@ from congruence import Congruence
 
 class CongruenceTests(unittest.TestCase):
 
-    def test_first(self):
-        self.assertEqual(0, 0)
-        self.assertTrue(True)
+    def test_user_friendly(self):
+        raw = tuple([3, 7])
+        mod = Congruence(3, 7)
+        self.assertEqual("3 (mod 7)", str(mod))
+        self.assertEqual(repr(raw), repr(mod))
