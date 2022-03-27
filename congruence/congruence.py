32d9530fb513f2224319fc3eaaf731974b85f4f0
carrvo <carrvo@gmail.com>
2022-03-27 08:48:53 -0600

    derive from tuple built-in

--- congruence/congruence.py
+++ congruence/congruence.py
@@ -1,7 +1,13 @@
 """
 """
 
-class Congruence(object):
+class Congruence(tuple):
     """
     """
-    pass
+
+    def __new__(cls, value, modulus):
+        """
+        :kudos: to https://stackoverflow.com/a/23878346
+        """
+        self = super(Congruence, cls).__new__(cls, [value, modulus])
+        return self
