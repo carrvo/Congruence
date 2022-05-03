6147c221682071dbae673349aa04c92a8471d75d
carrvo <carrvo@gmail.com>
2022-05-02 23:40:36 -0600

    removal of redundant code and overlapping theory

--- congruence/congruence_class.py
+++ congruence/congruence_class.py
@@ -5,17 +5,12 @@ import sys
 
 import congruence
 
-class CongruenceClass(object):
+class CongruenceClass(congruence.Congruence):
     """
     """
 
     def __init__(self, value, modulus):
-        modulus = abs(modulus)
-        self.remainder = value % modulus
-        self.modulus = modulus
-
-    def __repr__(self):
-        return repr(tuple([self.remainder, self.modulus]))
+        super().__init__(value, modulus)
 
     def __str__(self):
         return f'{self.remainder}\u0304'
@@ -46,55 +41,6 @@ class CongruenceClass(object):
             raise ValueError(f"{other} from {type(other)} is not of "
                              f"int for which to compare (mod {self.modulus})")
         return other % self.modulus == self.remainder
-
-    def __sanitize(self, other):
-        if not isinstance(other, self.__class__):
-            return self.__class__(other, self.modulus)
-        if other.modulus != self.modulus:
-            raise ValueError(f"modulus {other.modulus} from {type(other)} "
-                             f"differs against {self.modulus} from {type(self)}")
-        return other
-
-    def __eq__(self, other):
-        other = self.__sanitize(other)
-        return (self.modulus == other.modulus and
-                self.remainder == other.remainder)
-
-    def __add__(self, other):
-        other = self.__sanitize(other)
-        return self.__class__(self.remainder + other.remainder, self.modulus)
-
-    __radd__ = __add__
-
-    def __sub__(self, other):
-        other = self.__sanitize(other)
-        return self.__class__(self.remainder - other.remainder, self.modulus)
-
-    __rsub__ = __sub__
-
-    def __mul__(self, other):
-        other = self.__sanitize(other)
-        return self.__class__(self.remainder * other.remainder, self.modulus)
-
-    __rmul__ = __mul__
-
-    @property
-    def is_relatively_prime_to_modulus(self):
-        return congruence.is_relatively_prime(self.remainder, self.modulus)
-
-    @property
-    def multiplicative_inverse(self):
-        if not self.is_relatively_prime_to_modulus:
-            raise ValueError(f"{str(self)} must be relatively prime"
-                             f" to {self.modulus}")
-        # brute force but limited to the range it must exist within
-        for multiple in range(1, self.remainder+1):
-            condition = multiple * self.modulus + 1
-            value, mod = divmod(condition, self.remainder)
-            if mod == 0:
-                return self.__class__(value, self.modulus)
-        else:
-            raise ValueError(f"No multiplicative inverse for {str(self)}")
 
 def Zm(m):
     return CongruenceClass.Set(m)
