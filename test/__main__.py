8d300ee42db8eb2d3e58c796a3b0efa598245da7
carrvo <carrvo@gmail.com>
2022-06-08 20:24:57 -0600

    fix unittest
    allows for the use of
    `py -3 -m test`
    over the use of
    `py -3 test`

--- test/__main__.py
+++ test/__main__.py
@@ -1,11 +1,5 @@
 import unittest
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.curdir))
-
-import test
-from test_congruence import CongruenceTests
-from test_congruence_class import CongruenceClassTests
+from test import *
 
 unittest.main()
